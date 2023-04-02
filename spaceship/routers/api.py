ef29dfb7b920283eff7f47f818fa77d6a548c1d7
Alina Dubchak <alina.dubchak.ev@gmail.com>
2023-04-02 16:19:46 +0300

    The matrix product endpoint has been implemented The following files were changed in the process: Dockerfile - installiation required dependencies requirements/backend.in - added dependecies spaceship/routers/api.py -added matrix product endpoint

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
 
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'You have unlocked a new achievement in docker!'}
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_A = numpy.random.rand(10, 10)
+    matrix_B = numpy.random.rand(10, 10)
+    product = numpy.matmul(matrix_A, matrix_B)
+
+    result = {
+        "matrix_A": matrix_A.tolist(),
+        "matrix_B": matrix_B.tolist(),
+        "product of matrices": product.tolist()
+    }
+
+    return result
